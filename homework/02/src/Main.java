89e32736c1abd2680d3d2eddaeef6a981f2e387f
s88b <bell3@yandex.ru>
2021-11-20 18:44:19 +0300

    Изменена структура проекта: добавлена директория homework

---
+++ homework/02/src/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        int a = 2, b = 3, c = 4, d = 5, e = 6, f = 7,  j = 8,  h = 9 ;
+        int result = (a + b + c + d + e + f + j + h) / 8 ;
+
+        System.out.println(result);
+    }
+}
