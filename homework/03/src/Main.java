740fb973f49df8c538d3632646684282bc3a5dbb
s88b <bell3@yandex.ru>
2021-12-07 21:25:39 +0300

    Добавлено домашнее задание 03

---
+++ homework/03/src/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
