befa8f03fcb55976d2abadb605dc17c14499399c
s88b <bell3@yandex.ru>
2021-12-07 21:26:42 +0300

    Добавлено домашнее задание 04

---
+++ homework/04/src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        int[] arr = new int[]{1, 2, 3};
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + 5;
+            System.out.println(arr[i]);
+        }
+    }
+}
